513ee5b155a200f2c4d713763333c8bf2cdf8467
c0mplexdevelop <snakebigbosslel@gmail.com>
2022-11-21 17:09:03 +0800

    Added the main for the jar files

---
+++ src/main/java/cc102/game/Main.java
@@ -0,0 +1,8 @@
+package cc102.game;
+
+public class Main {
+    public static void main(String[] args) {
+        // Jar files dont like running classes that inherit sutff, so we do it in a separate main file!
+        Tutorial3.main(args);
+    }
+}
