457c311e3cba5a70e9c63d2c763dca16d1e2ea38
c0mplexdevelop <snakebigbosslel@gmail.com>
2022-11-29 07:59:43 +0800

    Fixed the disappearance of currentTurn and gameOverMessage. Added the lineDrawForLeftDiagonal.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module cc102.game {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
-
+    
     opens cc102.game to javafx.fxml;
     exports cc102.game;
 }
