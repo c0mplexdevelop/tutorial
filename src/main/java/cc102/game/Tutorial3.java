9c150685f71b93255035b96fa048f1cdff899d08
c0mplexdevelop <snakebigbosslel@gmail.com>
2022-12-02 16:43:25 +0800

    Front-end improvements, some refactoring, removal of repetitive and unneccessary code

--- src/main/java/cc102/game/Tutorial3.java
+++ src/main/java/cc102/game/Tutorial3.java
@@ -20,6 +20,8 @@ import java.util.Arrays;
 public class Tutorial3 extends Application {
     static String currentTurn = "X";
     static Text currentTurnText = new Text(0, 0, String.format("Current Turn: %s", currentTurn));
+    private static Color backgroudColor = Color.PINK;
+    private static Color winningLineColor = Color.web("#C25A7C");
     Board board = new Board(createBoard());
     List<Rectangle> rects = new ArrayList<Rectangle>();
     String winningPlayer = null;
@@ -56,15 +58,12 @@ public class Tutorial3 extends Application {
             }
         });
 
-        /*
-         * currentTurnText.setFill(Color.RED);
-         * currentTurnText.setTextOrigin(VPos.TOP);
-         * currentTurnText.setFont(Font.font("Times New Roman", 30));
-         */
-
+        
         setCurrentTurn(currentTurnText, "X");
+
+        currentTurnText.setFill(Color.BLACK);
     
-        gameOverMessage.setFill(Color.RED);
+        gameOverMessage.setFill(Color.BLACK);
         gameOverMessage.setTextOrigin(VPos.TOP);
         gameOverMessage.setFont(Font.font("Times New Roman", 25));
 
@@ -74,6 +73,7 @@ public class Tutorial3 extends Application {
         // root.getChildren().addAll(rects);
         root.getChildren().add(currentTurnText);
         root.getChildren().add(gameOverMessage);
+        primaryScene.setFill(backgroudColor);
         primaryStage.setScene(primaryScene);
         primaryStage.show();
 
@@ -96,16 +96,17 @@ public class Tutorial3 extends Application {
             for(int col = 0; col < 3; col++) {
                 // Rect takes the first 2 arguments as WIDTH AND LENGTH, with the last one as Color.
                 Rectangle rect = new Rectangle(scene.getWidth()/3, 600/3);
+                rect.setFill(backgroudColor);
 
                 final Text boardMove = new Text("X");
-                boardMove.setFill(Color.RED);
+                boardMove.setFill(Color.BLACK);
                 
                 boardMove.setFont(Font.font("Times New Roman", 50));
 
                 rect.setX(scene.getWidth()/3 * col);
                 rect.setY(600/3 * row + 100);
-                // Set the border color RED
-                rect.setStroke(Color.RED);
+                // Set the border color ROGUE PINK. Credits to: Ysa
+                rect.setStroke(Color.web("#C12869"));
                 //set the thickness of the border, i.e. A Strok.
                 rect.setStrokeWidth(1);
 
@@ -158,7 +159,6 @@ public class Tutorial3 extends Application {
 
     public static void setCurrentTurn(Text currentTurn, String turn) {
         currentTurn.setText(String.format("Current Turn: %s", turn));
-        currentTurn.setFill(Color.RED);
         currentTurn.setTextOrigin(VPos.TOP);
         currentTurn.setFont(Font.font("Times New Roman", 30));
     }
@@ -176,6 +176,7 @@ public class Tutorial3 extends Application {
     }
 
     public void restart(Pane root, Scene primaryScene, Text gameOverMessage) {
+        primaryScene.setFill(Color.PINK);
         // Update the currentTurnText properly, as if X won, it remains as O after restart
         setCurrentTurn(currentTurnText, currentTurn);
         // We just need to re-add the gameOverMessage and currentTurnText
@@ -224,7 +225,7 @@ public class Tutorial3 extends Application {
         double rectHalfWidth = 100;
 
         Line winningLine = new Line(0, beginningPosition+rectHalfWidth, scene.getWidth(), beginningPosition+rectHalfWidth);
-        winningLine.setStroke(Color.LIGHTBLUE);
+        winningLine.setStroke(winningLineColor);
         winningLine.setStrokeWidth(10);
         winningLine.toBack();
         
@@ -273,7 +274,7 @@ public class Tutorial3 extends Application {
         Line winningLine = new Line(beginningPosition + rectHalfWidth + 3, 105 , beginningPosition + rectHalfWidth + 3, primaryScene.getHeight());
         // Its 105 since we put it within the rectangle, its probably like (Stroke Width / # of suqares) + rect stroke width + 1
         // For the above, we added +3 to centralize the line stroke on the center. Essentially, just do strokeWidth/3
-        winningLine.setStroke(Color.LIGHTBLUE);
+        winningLine.setStroke(winningLineColor);
         winningLine.setStrokeWidth(10);
         winningLine.toBack();
 
@@ -304,7 +305,7 @@ public class Tutorial3 extends Application {
                 primaryScene.getHeight());
 
         winningLine.setStrokeWidth(10);
-        winningLine.setStroke(Color.LIGHTBLUE);
+        winningLine.setStroke(winningLineColor);
         winningLine.toBack();
 
         root.getChildren().add(winningLine);
@@ -335,7 +336,7 @@ public class Tutorial3 extends Application {
                 primaryScene.getHeight());
 
         winningLine.setStrokeWidth(10);
-        winningLine.setStroke(Color.LIGHTBLUE);
+        winningLine.setStroke(winningLineColor);
         winningLine.toBack();
 
         root.getChildren().add(winningLine);
